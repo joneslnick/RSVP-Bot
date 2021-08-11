8ea5b860d6d84a36884648c144c6dc0aa0d41ed3
joneslnick <38118499+joneslnick@users.noreply.github.com>
2021-08-10 22:21:14 -0400

    Initial Upload
    Provides basic features, including start/stop with reading to/from disk

---
+++ Event.py
@@ -0,0 +1,91 @@
+from datetime import datetime, time, timedelta
+from discord.utils import get
+import emoji
+
+DEFAULT_EXPIRE = 3 #Default days of expiration
+
+class Event:
+    def __init__(self, id_num, ctx, author, args, expire=None):
+        self.id = id_num
+        self.ctx = ctx
+        self.author = author
+        self.args = args
+        self.message = None
+        self.reactions = [] #List of (Reaction, User) Tuples
+        self.rsvp_list = [] #Users who have been asked to rsvp
+        self.status = {}
+        self.expire = expire
+
+        for arg in args:
+            if arg == "req":
+                var = " ".join(args[arg])
+                if get(ctx.guild.roles, name=var): #Name passed is a group
+                    for member in get(ctx.guild.roles, name=var).members:
+                        self.rsvp_list.append(member)
+            if arg == "expire" and self.expire is None:
+                try:
+                    self.expire = datetime.strptime(" ".join(args[arg]), "%m/%d/%Y %H:%M")
+                except:
+                    args["expire"] = datetime.now() + timedelta(days = DEFAULT_EXPIRE)
+                    self.expire = args["expire"]
+        
+        for member in self.rsvp_list:
+            self.status[member] = "grey_question"
+        
+
+    async def selfDestruct(self):
+        await self.message.delete() #Remove message on destruction
+    
+    async def ProcessReactions(self):
+        for reaction in self.reactions:
+            if reaction[1] not in self.rsvp_list: #Users reaction is not important
+                pass
+
+            elif reaction[0].custom_emoji:
+                pass
+
+            else:
+                demoji = emoji.demojize(str(reaction[0]))
+                if demoji in [":check_mark_button:", ":cross_mark:"]:
+                    if demoji == ":check_mark_button:":
+                        self.status[reaction[1]] = "white_check_mark"
+                    elif demoji == ":cross_mark:":
+                        self.status[reaction[1]] = "x"
+        
+        
+        await self.message.edit(content=self.CreateString())
+
+    async def ImportReact(self):
+        if self.message is not None:
+            for reaction in self.message.reactions:
+                async for user in reaction.users():
+                    self.reactions.append((reaction,user))
+
+    def CreateString(self):
+        sendString = f"{self.author} is creating an event: \n"
+        sendString += "----------------------------------\n"
+        for arg in self.args:
+            if arg == "message": #User is adding a message
+                print("Adding a message")
+                sendString += "**" + " ".join(self.args[arg]) + "**\n" #Append any message, such as "Climbing at ET on Saturday 0900"
+
+            elif arg == "req": #User is requesting certain users or groups to be pinged
+                sendString += " They have requested that the following individuals respond:\n"
+                var = " ".join(self.args[arg])
+                if get(self.ctx.guild.roles, name=var): #Name passed is a group
+                    for member in get(self.ctx.guild.roles, name=var).members:
+                        sendString += f"\t\t {member.mention} \t:{self.status.get(member)}:\n"
+
+        return sendString
+    
+    def JsonDump(self):
+        dictionary = {
+            self.id: {
+                "guild_id": self.ctx.guild.id,
+                "channel_id": self.ctx.channel.id,
+                "message_id": self.message.id,
+                "author_id": self.author.id,
+                "args": self.args,
+            }
+        }
+        return dictionary
---
+++ RSVP-Bot.py
@@ -0,0 +1,182 @@
+from json.decoder import JSONDecodeError
+import discord, json
+from discord.ext import commands, tasks
+from discord.utils import get
+from Event import Event
+import emoji
+from datetime import datetime
+
+
+
+description = "Manages RSVP's. Pings all requested users and tracks responses."
+intents = discord.Intents.default()
+intents.members = True
+intents.reactions = True
+intents.emojis = True
+
+
+BOT = commands.Bot(command_prefix='/', description=description, intents=intents)
+ID_NUM = 0 #Each event is assigned a unique ID for record keeping (incr by one)
+EVENTS = []
+LOG_FILE = "logfile.txt"
+
+class ExpireCog(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+        self.CheckTimeout.start()
+    
+    @tasks.loop(seconds=60)
+    async def CheckTimeout(self):
+        global EVENTS
+
+        print("Checking if any events have expired")
+        for event in EVENTS:
+            if (datetime.now() > event.expire): #Event has expired
+                await event.selfDestruct()
+                EVENTS.remove(event)
+        Export(LOG_FILE)
+
+@BOT.event
+async def on_ready():
+    print(f"Logged in as {BOT.user.name}")
+    await Import(LOG_FILE)
+    ExpireCog(BOT)
+
+@BOT.command(name="rsvp", pass_context=True)
+async def CreateEvent(ctx):
+    global EVENTS
+    global ID_NUM
+
+    args = ArgParser(ctx.message.content)
+    await ctx.message.delete() #Delete the command request message
+
+    newEvent = Event(ID_NUM, ctx, ctx.author, args)
+    EVENTS.append(newEvent)
+    newEvent.message = await ctx.send(newEvent.CreateString())    
+    ID_NUM += 1
+    Export(LOG_FILE)        
+
+@BOT.event
+async def on_raw_reaction_add(payload):
+    global EVENTS
+
+    msg = await BOT.get_channel(payload.channel_id).fetch_message(payload.message_id)
+    reaction = get(msg.reactions, emoji=payload.emoji.name)
+    user = payload.member
+    
+    print(reaction, user)
+    if (user and reaction):
+        for event in EVENTS:
+            if event.message.id == msg.id: #Reaction was added to a valid comp event
+                print(f"User {user} has reacted with {payload.emoji}")
+                event.reactions.append((reaction,user)) #Add reaction to list of reactions for specific event
+                
+            await event.ProcessReactions()
+
+
+@BOT.event
+async def on_raw_reaction_remove(payload):
+
+    msg = await BOT.get_channel(payload.channel_id).fetch_message(payload.message_id)
+    guild = BOT.get_guild(payload.guild_id)
+
+    if guild is None:
+        return
+    
+    user = guild.get_member(payload.user_id)
+
+    if user is not None:
+        for event in EVENTS:
+            if event.message.id == msg.id: #Reaction was removed from a valid comp event
+                print(f"User {user} has removed their reaction of {payload.emoji}")
+                for react in event.reactions:
+                    if emoji.demojize(str(react[0])) == emoji.demojize(payload.emoji.name) and react[1] == user:
+                        print("Removed a Reaction")
+                        event.reactions.remove(react) #Remove reaction from reaction list
+                        event.status[user] = "grey_question" #Until reprocessed, assume the user removed all roles
+
+            await event.ProcessReactions()
+        
+def ArgParser(command):
+    argsParsed = {}
+    cmd = command.split()
+    for word in cmd[1:]:
+        index = cmd.index(word)
+        if word.startswith('--'):
+            args = []
+            try:
+                for var in cmd[index+1:]:
+                    if not var.startswith('--'):
+                        args.append(var)
+                    else:
+                        break
+                argsParsed[word[2:]] = args
+
+            except IndexError:
+                argsParsed[word[2:]] = args
+
+    return argsParsed
+
+def RetrieveToken():
+    #Definately better ways to handle this
+    with open("DISCORD_TOKEN.txt","r") as file:
+        token = file.read()
+        file.close()
+    
+    return token
+
+def Export(fileName):
+    jsonString = ""
+
+    with open(fileName, "w") as f:
+        for event in EVENTS:
+            json.dump(event.JsonDump(), f)
+    print("Completed exporting to file.")
+
+async def Import(fileName):
+    global EVENTS
+    EVENTS.clear()
+
+    try:
+        with open(fileName, "r") as f:
+            jsonString = json.load(f)
+
+            for event in jsonString:
+                event_id = int(event)
+                guild_id = int(jsonString[event]["guild_id"])
+                channel_id = int(jsonString[event]["channel_id"])
+                message_id = int(jsonString[event]["message_id"])
+                if guild_id is not None:
+                    guild = BOT.get_guild(guild_id)
+                    if guild is not None:
+                        author = await guild.fetch_member(jsonString[event]["author_id"])
+
+                args = {}
+                for flag in jsonString[event]["args"]:
+                    args[flag] = jsonString[event]["args"][flag]
+                
+                expire = datetime.strptime(" ".join(jsonString[event]["args"]["expire"]), "%m/%d/%Y %H:%M")
+                channel = BOT.get_channel(channel_id)
+                message = await channel.fetch_message(message_id)
+
+                ctx = await BOT.get_context(message)
+                newEvent = Event(event_id, ctx, author, args, expire)
+                newEvent.message = message
+                await newEvent.ImportReact() #Check for any reactions after we set the message
+                await newEvent.ProcessReactions()
+
+                EVENTS.append(newEvent)
+
+    except FileNotFoundError:
+        pass
+    except JSONDecodeError:
+        pass
+
+    print("Completed import from file.")
+
+def main():
+    BOT.run(RetrieveToken())
+ 
+
+if __name__ == '__main__':
+    main()
